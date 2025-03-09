6242f3e2b437dc4e861dacf844a8bcb940a4cb06
mokhtarahmed123 <165895390+mokhtarahmed123@users.noreply.github.com>
2025-03-09 23:00:24 +0200

    Add files via upload

---
+++ Sheet1Assuit.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using static System.Math;
+
+namespace Sheet1Assiut
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region A
+            ////Console.WriteLine("enter a name ");
+            //string name = Console.ReadLine();
+            ////string print = $"hello  {name}";
+            //Console.WriteLine("hello" + name);
+            #endregion
+            #region B   
+            //Console.WriteLine(" enter your string");
+            //string input = Console.ReadLine();
+            //string output = input.Replace(' ', '\n');
+            //Console.WriteLine(output);
+            #endregion
+            #region C
+            // Console.WriteLine(" enter a first and second numbers");
+            // int number1 = int.Parse(Console.ReadLine());
+            // int number2 = int.Parse(Console.ReadLine());
+            // Console.WriteLine("enter a char");
+            // char c = char.Parse(Console.ReadLine());
+            //// int result = number1 + number2;
+            // Console.WriteLine(number1 + number2);
+            // Console.WriteLine(number1 - number2);
+            // Console.WriteLine(number1 * number2);
+            #endregion
+            #region E
+            //const decimal  PI = 3.14M;
+            //Console.WriteLine("enter a raduis to calculate an Area");
+            //double raduis  = double.Parse(Console.ReadLine());
+            //decimal Area  = (decimal)(3.141592653 * raduis * raduis);
+            ////decimal b = 
+            //Console.WriteLine(Area);
+
+            #endregion
+            #region D
+            //Console.WriteLine(" enter a A,B,C,D");
+            //int A =int.Parse (Console.ReadLine());
+            //int B =int.Parse (Console.ReadLine());
+            //int C =int.Parse (Console.ReadLine());
+            //int D =int.Parse (Console.ReadLine());
+            //int X = (A * B) - (C * D);
+            //Console.WriteLine(X);
+
+            #endregion
+            #region F
+            //Console.WriteLine(" enter a first number");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" enter a second number");
+            //int num2 = int.Parse(Console.ReadLine());
+            ////string S1 = new string(num1);
+            //string s1 = Convert.ToString(num1);
+            //string s2 = Convert.ToString(num2);
+            //char n1 = s1[s1.Length - 1];
+            //char n2 = s2[s2.Length - 1];
+            ////int r = Convert.ToInt32(n1) + Convert.ToInt32(n2);
+            //int c = n1-'0';
+            //int c1 = n2 - '0'; ;
+            ////Console.WriteLine(n1);
+            ////Console.WriteLine(n2);
+            ////Console.WriteLine(r);
+            //int result = c + c1;
+            //Console.WriteLine(result);
+
+
+            #endregion
+            #region G
+            //Console.WriteLine(Summation(10));
+            //static int Summation(int n)
+            //{
+            //    if (n == 0) return 0;
+            //    return n + Summation(n - 1);
+
+            //}
+            #endregion
+            #region H
+            //Console.WriteLine(" enter num 1 ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" enter num 2 ");
+            //int num2 = int.Parse(Console.ReadLine());
+            //double c = System.Math.Ceiling((double)num1 / num2); 
+            //double F = System.Math.Floor((double)num1 / num2); 
+            //double R = System.Math.Round((double)num1 / num2); 
+
+            //Console.WriteLine(c);
+            //Console.WriteLine(F);
+            //Console.WriteLine(R);
+            #endregion
+            #region I
+            //Console.WriteLine(" enter the first number");
+            //int num1 = int.Parse(Console.ReadLine());  
+            //Console.WriteLine(" enter the second number");
+            //int num2 = int.Parse(Console.ReadLine());
+            //if (num1 >= num2)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+            //string s = num1 > num2 ? "Yes" : "NO";
+            //Console.WriteLine(s);
+            #endregion
+            #region J
+            //Console.WriteLine(" enter the first number");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" enter the second number");
+            //int num2 = int.Parse(Console.ReadLine());
+            //string Result = num1 % num2 == 0 ? "Multiples" : "NO Multiples";
+            //Console.WriteLine(Result);
+            #endregion
+            #region  K
+            //Console.WriteLine(" enter the first number");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" enter the second number");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" enter the second number");
+            //int num3 = int.Parse(Console.ReadLine());
+            //List<int> list = new List<int>();
+            //list.Add(num1);
+            //list.Add(num2);
+            //list.Add(num3);
+            //list.Sort();
+            //Console.WriteLine(list[0]);
+            //Console.WriteLine(list[list.Count -1]);
+            #endregion
+            #region L
+            Console.WriteLine("Enter A first name for first person");
+            string F1 = Console.ReadLine();   
+            Console.WriteLine("Enter A Second name for first person");
+            string S1 = Console.ReadLine();  
+            Console.WriteLine("Enter A first name for second person");
+            string F2 = Console.ReadLine();   
+            Console.WriteLine("Enter A Second name for second person");
+            string S2 = Console.ReadLine(); 
+            bool result = S2 == S1 
+
+            #endregion
+
+
+        }
+
+    }
+}
